2e2d12da05d11b668c58a43542b0dc31fb8eaf35
kiran kamal <chauhankirankamal090@gmail.com>
2025-08-30 02:18:10 +0530

    core js concept practice

---
+++ closure-ex.js
@@ -0,0 +1,18 @@
+//  • Practice: Write a closure that remembers a user’s role and returns different permissions.
+function createUserRole(role) {
+    return function getPermissions() {
+        if (role === 'admin') {
+            return 'All permissions granted.';
+        } else if (role === 'editor') {
+            return 'Edit permissions granted.';
+        } else if (role === 'viewer') {
+            return 'View permissions granted.';
+        } else {
+            return 'No permissions granted.';
+        }
+    }
+}
+const adminPermissions = createUserRole('admin');
+console.log(adminPermissions()); // 
+// Output: All permissions granted
+
